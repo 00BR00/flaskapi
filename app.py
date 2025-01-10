4cf0d3310cd526167c7f8010268c1fa4a0320673
00BR00 <149963335+00BR00@users.noreply.github.com>
2025-01-10 18:12:37 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, render_template_string, request
+
+app = Flask(__name__)
+
+systems_mapping = {
+    "navigation": "NAV-01",
+    "communications": "COM-02",
+    "life_support": "LIFE-03",
+    "engines": "ENG-04",
+    "deflector_shield": "SHLD-05"
+}
+
+damaged_system = "engines"  # Puedes cambiar esto o hacerlo aleatorio si prefieres
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({"damaged_system": damaged_system})
+
+@app.route('/repair-bay', methods=['GET'])
+def repair_bay():
+    code = systems_mapping.get(damaged_system, "UNKNOWN")
+    html_template = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Repair</title>
+    </head>
+    <body>
+    <div class="anchor-point">{{ code }}</div>
+    </body>
+    </html>
+    '''
+    return render_template_string(html_template, code=code)
+
+@app.route('/teapot', methods=['POST'])
+def teapot():
+    return "I'm a teapot", 418
+
+if __name__ == '__main__':
+    # Cambiar 'debug' a False en un entorno de producción
+    app.run(host='0.0.0.0', port=5000, debug=True)
